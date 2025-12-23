5d2fdf64ce7aef76c9791cd868d0c40f75783c16
devgsanches <guilherme.sanches@flapper.aero>
2025-12-22 22:24:23 -0300

    chore: configure testing environment with happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    setupFiles: ['./vitest/setup.ts'],
-    environment: 'jsdom'
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom'
   }
 })
