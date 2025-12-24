ddce6a05c18ffe6db8bb2a5887917f1b6d4b9ceb
devgsanches <guilherme.sanches@flapper.aero>
2025-12-24 15:57:03 -0300

    fix: small ajust in coverage script

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,10 +13,7 @@ export default defineConfig({
     environment: 'happy-dom',
     coverage: {
       provider: 'v8',
-      include: [
-        'app/**/_components/**/*.{ts,tsx}',
-        'app/**/components/**/*.{spec,test}.{ts,tsx,js,jsx}'
-      ],
+      include: ['app/**/_components/**/*.{ts,tsx}'],
       exclude: ['app/**/components/ui/**/*.{ts,tsx,js,jsx}']
     },
     include: ['**/*.{test,spec}.{ts,tsx,js,jsx}']
